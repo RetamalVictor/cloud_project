6e0bf64a1b475ce7290f4c45ea303dd270a14ba1
Victor Retamal <99215206+RetamalVictor@users.noreply.github.com>
2022-03-15 15:58:34 +0100

    test again

--- test_hello.py
+++ test_hello.py
@@ -1,13 +1,7 @@
-from hello import *
-
-def setup_funciton(function):
-    print("Running Setup: %s" %{function.__name__})
-    function.x = 10
-def teardowm_function(function):
-    print("Running Teardown: %s:" %{function.__name__})
+from hello import subtract
 
 #def test_hello_add():
     #assert add(test_hello_add.x) == 11
 
 def test_hello_subtract():
-    assert subtract(test_hello_subtract.x) == 9
+    assert subtract(10) == 9
