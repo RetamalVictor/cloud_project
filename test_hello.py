01edf020df7422c2a027ca11b2bdaf262a067da7
Victor Retamal <99215206+RetamalVictor@users.noreply.github.com>
2022-03-15 13:56:34 +0100

    Added files

---
+++ test_hello.py
@@ -0,0 +1,13 @@
+from hello import *
+
+def setup_funciton(function):
+    print("Running Setup: %s" %{function.__name__})
+    function.x = 10
+def teardowm_function(function):
+    print("Running Teardown: %s:" %{function.__name__})
+
+def test_hello_add():
+    assert add(test_hello_add.x) == 11
+
+def test_hello_subtract():
+    assert subtract(test_hello_subtract.x) == 9
